1f1f40ab3005147411b46218603b7255546d9239
MulualemBerhanu <your.email@example.com>
2025-02-12 21:53:11 -0800

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    loader: 'custom',
+    loaderFile: './image-loader.js',
   },
-  basePath: '/Genesisgrouphome',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig 
